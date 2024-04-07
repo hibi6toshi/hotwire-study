9bfcc17501296e8fbacc487256936f6d16baefde
Hidetoshi Hibino <hibinozzzz@gmail.com>
2024-04-07 21:37:02 +0900

    検索機能の追加

--- Gemfile
+++ Gemfile
@@ -38,6 +38,9 @@ gem "cssbundling-rails"
 # ページネーション
 gem "kaminari"
 
+# 検索機能
+gem "ransack"
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ windows jruby ]
 
--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -3,7 +3,12 @@ class CatsController < ApplicationController
 
   # GET /cats
   def index
-    @cats = Cat.page(params[:page])
+    @search = Cat.ransack(params[:q])
+
+    # デフォルトのソートをid降順にする
+    @search.sorts = 'id desc' if @search.sorts.empty?
+
+    @cats = @search.result.page(params[:page])
   end
 
   # GET /cats/1
